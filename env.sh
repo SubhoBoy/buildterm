118a6deaacf782ca3ea4a433b041a1c7a7a53205
root <root@localhost.localdomain>
2026-01-28 16:53:39 +0000

    Enviroment file

---
+++ env.sh
@@ -0,0 +1,8 @@
+# Adjust the paths!
+export ANDROIDSDK="/root/calculator/apk/sdk"
+export ANDROID_HOME="/root/calculator/apk/sdk"
+export ANDROIDNDK="/root/calculator/apk/android-ndk-r25c"
+export ANDROIDAPI="27"
+export NDKAPI="21"
+export ANDROIDNDKVER="r25c"
+export PATH="$ANDROIDSDK/tools/bin:$PATH"
